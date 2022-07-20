7bc400fe09acd70d28a88dc0820a9eac7af77a21
zhengzhongpku <1500010705@pku.edu.cn>
2022-07-20 16:01:19 -0400

    package update

--- Rpackage_GeoNetParallel/Example/testing_script_0621.R
+++ Rpackage_GeoNetParallel/Example/testing_script_0621.R
@@ -1,5 +1,5 @@
 setwd("/home/handslab/Desktop/zhong_zheng/test_envir_medium_all_0621")
-library(StreamNet)
+library(GeoNetParallel)
 ############test whole pipeline################################
 path_total = "/home/handslab/Desktop/zhong_zheng/test_envir_medium_all_0621/test_envir_whole_pipe/"
 filename_chem = "Water_Chemistry_R_Package.csv"
